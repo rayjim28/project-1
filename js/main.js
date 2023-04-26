3ef0161fa8d96e6d2a9675e950c152cd63414c5b
rayjim28 <rjimenez1030@icloud.com>
2023-04-26 11:02:11 -0400

    fixing bugs and some css for color

--- js/main.js
+++ js/main.js
@@ -1,177 +1,148 @@
-// 1. Define required constants:
-/*----- constants -----*/
-const CATEGORIES = ["planets", "stars"];
+const CATEGORY = "planets";
+const WORDS = [
+  "mercury",
+  "venus",
+  "earth",
+  "mars",
+  "jupiter",
+  "saturn",
+  "uranus",
+  "neptune",
+];
 const MAX_GUESSES = 6;
-const WORDS = {
-  planets: [
-    "mercury",
-    "venus",
-    "earth",
-    "mars",
-    "jupiter",
-    "saturn",
-    "uranus",
-    "neptune",
-  ],
-
-  stars: [
-    "sirius",
-    "canopus",
-    "arcturus",
-    "vega",
-    "rigel",
-    "procyon",
-    "mimosa",
-    "spica",
-    "pollux",
-  ],
-};
-const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-
-/*----- state variables -----*/
-// 2. Define required variables used to track the state of the game:
+const ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
 
 let gameBoard = [];
 let chosenCategory;
-let hiddenWord = "";
-let currentLetter;
-let correctGuesses = new Set();
-let incorrectGuesses = 0;
-let gameEnd = false;
+let hiddenWord;
+let correctGuesses;
+let incorrectGuesses;
+let gameResults;
+let gameEnds;
 
-/*----- cached elements  -----*/
-// 3. Cache DOM elements:
-
-const gameContainer = document.getElementById("game-container");
-const categorySelect = document.getElementById("category-select");
+const categorySelected = document.getElementById("category-select");
 const wordDisplay = document.getElementById("word-display");
 const keyboard = document.getElementById("keyboard");
-const letterBtn = document.querySelectorAll(".alphabet");
-const resultContainer = document.getElementById("result-container");
-const replayBtn = document.getElementById("replay");
-const spacemanParts = document.querySelectorAll("#spaceman > div");
-const headEl = document.getElementById("head");
-const bodyEL = document.getElementById("body");
-const leftArmEl = document.getElementById("left-arm");
-const rightArmEl = document.getElementById("right-arm");
-const leftLegEl = document.getElementById("left-leg");
-const rightLegEl = document.getElementById("right-leg");
+const lettersButton = document.querySelectorAll(".alphabet");
+const spacemanSvg = document.getElementById("spaceman-container");
+const head = document.querySelector(".head");
+const bodyParts = document.querySelectorAll(".body-part");
+const resultCon = document.getElementById("result-container");
+const gameMessage = document.getElementById("game-results");
+const newGameBtn = document.getElementById("replay");
 
-/*----- event listeners -----*/
 
-// add event listener to the letter button
-letterBtn.forEach((letter) => {
+lettersButton.forEach((letter) => {
   letter.addEventListener("click", handleLetterClick);
+  letter.disabled = "true";
 });
 
-replayBtn.addEventListener("click", handleReplay);
+newGameBtn.addEventListener("click", handleReplay);
 
-/*----- functions -----*/
-// 4. Upon loading the app should: 4.1) Initialize the state variables
 
-function init() {
-  chosenCategory = CATEGORIES[0];
+function inIt() {
+  chosenCategory = CATEGORY[0];
   correctGuesses = new Set();
   incorrectGuesses = 0;
 
-  // choose a random word from the chosen category
-  let wordsInCategory = WORDS[chosenCategory];
-  hiddenWord =
-    wordsInCategory[Math.floor(Math.random() * wordsInCategory.length)];
+  hiddenWord = WORDS[Math.floor(Math.random() * WORDS.length)];
 
-  // update the display to show the lines for each letter
   wordDisplay.textContent = hiddenWord
     .split("")
     .map((letter) => (correctGuesses.has(letter) ? letter : "_"))
     .join(" ");
+}
+inIt();
+console.log(hiddenWord);
+console.log(WORDS);
 
-  resultContainer.classList.add("result-container-hide");
+function UpdateSpaceman() {
+  const numIncorrectGuesses = incorrectGuesses - 1;
+  for (let i = 0; i <= numIncorrectGuesses; i++) {
+    const bodyPart = bodyParts[i];
+    if (bodyPart) {
+    bodyParts[i].classList.remove("hidden");
+    }
+  }
 }
 
-// 5. Handle a player clicking a button of a letter:
+function showBodyPart(bodyPart) {
+  const element = document.getElementById(bodyPart);
+  if (element) {
+  element.classList.remove("hidden");
+  }
+}
+
 
 function handleLetterClick(e) {
   let letter = e.target.textContent;
   console.log(`handleLetterClick: ${letter}`);
-  // checking if letter has not been guessed
   if (!correctGuesses.has(letter)) {
     correctGuesses.add(letter);
     let userInput = hiddenWord
       .split("")
-      // .map((hiddenLetter) => (correctGuesses.has(letter) && hiddenLetter === letter ? letter : correctGuesses.has(hiddenLetter) ? hiddenLetter : "_"))
-      .map((hiddenLetter) =>
-        correctGuesses.has(hiddenLetter) ? hiddenLetter : "_"
+      .map((hiddenLetters) =>
+        correctGuesses.has(hiddenLetters) ? hiddenLetters : "_"
       )
       .join(" ");
-    // trying to update the word display to show the newly guessed letter
+    console.log(userInput);
+
     wordDisplay.textContent = userInput;
 
-    // checking if user/player won
     if (!userInput.includes("_")) {
-      gameEnd = true;
-      resultContainer.textContent = "You won!!, Great job!";
-      if (resultContainer.classList) {
-        resultContainer.classList.add("you-win");
+      gameEnds = true;
+      resultCon.textContent = "You Win!!, Great Job!";
+
+      if (resultCon.classList) {
+        resultCon.classList.add("you-win");
       }
-      // check if user/player lost
     } else if (!hiddenWord.includes(letter)) {
       incorrectGuesses++;
-      drawSpaceman(incorrectGuesses);
-
-      // check if the game is over
+      showBodyPart(`body-part-${incorrectGuesses}`);
+      if (incorrectGuesses === 1) {
+        showBodyPart('head');
+      } else if (incorrectGuesses === 2) {
+        showBodyPart('body');
+      } else if (incorrectGuesses === 3) {
+        showBodyPart('left-arm');
+      } else if (incorrectGuesses === 4) {
+        showBodyPart('right-arm');
+      } else if (incorrectGuesses === 5) {
+        showBodyPart('left-leg');
+      } else if (incorrectGuesses === 6) {
+        showBodyPart('right-leg');
+      }
       if (incorrectGuesses === MAX_GUESSES) {
-        gameEnd = true;
-        resultContainer.textContent = "You Lose!, Try Again";
-        if (resultContainer.classList) {
-          resultContainer.classList.add("you-lose!");
+        gameEnds = true;
+        resultCon.textContent = "You Lost Try Again";
+
+        if (resultCon.classList) {
+          resultCon.classList.add("you-lose");
         }
         wordDisplay.textContent = hiddenWord;
+      } else {
+        UpdateSpaceman(incorrectGuesses);
       }
     }
     console.log("userInput:", userInput);
   }
 }
 
-// 6. Handle a user/player clicking the replay button
-function handleReplay() {
-  gameEnd = false;
-  init();
-  resultContainer.classList.add("result-container-hide");
-}
-
-function drawSpaceman(incorrectGuesses) {
-  for (let i = 0; i < incorrectGuesses && i < spacemanParts.length; i++) {
-    spacemanParts[i].classList.add("show");
-  }
-
-  // if (incorrectGuesses > spacemanParts.length) {
-  //   return;
-  // }
-  // for (let i = 0; i < incorrectGuesses; i++) {
-  //   let parts = spacemanParts[i];
-  //   if (parts) {
-  //     parts.classList.add("show");
-  //   }
-  // }
-  // if (incorrectGuesses >= spacemanParts.indexOf(headEl)) {
-  //   headEl.classList.add("pop-out");
-  // }
-}
 
 function checkForWinner() {
   console.log(wordDisplay);
-  if (incorrectGuesses >= MAX_GUESSES) {
-    replayBtn.style.display = "block";
+  if (incorrectGuesses === MAX_GUESSES) {
+    newGameBtn.style.display = "block";
     return false;
   } else if (!wordDisplay.includes("_")) {
-    replayBtn.style.display = "block";
+    newGameBtn.style.display = "block";
     return true;
   } else {
     return false;
   }
 }
-checkForWinner();
 
-drawSpaceman();
-init();
-console.log();
+function handleReplay() {
+  gameEnds = false;
+  inIt()
+}
